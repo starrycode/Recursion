c8a63ecbe430974b89f6c0fed91073b2e41b0786
Violet <Jiye.Oh-1@ou.edu>
2020-11-09 16:17:56 -0600

    Init commit with Cake

---
+++ src/Cake.java
@@ -0,0 +1,17 @@
+
+public class Cake {
+	private int bites = 10;
+	
+	public Cake() {
+		// nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Hmm.. ");
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+	}
+}
